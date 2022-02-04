282c24e204799be4a248a9079d73b796d6634c83
xabie <xabiermikelgutierrez@gmail.com>
2022-02-04 13:05:17 +0100

    Posiblemente este terminado queda probarlo

--- Practica1botones/app/src/main/java/com/example/practica1botones/Activity2.java
+++ Practica1botones/app/src/main/java/com/example/practica1botones/Activity2.java
@@ -2,7 +2,9 @@ package com.example.practica1botones;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+    }
+
+    public void Ventana1(View view) {
+        finish();
+    }
+
+    public void Ventana3(View view) {
+        Intent intent = new Intent(this,Activity3.class);
+        startActivity(intent);
     }
 }
--- Practica1botones/app/src/main/java/com/example/practica1botones/Activity3.java
+++ Practica1botones/app/src/main/java/com/example/practica1botones/Activity3.java
@@ -2,20 +2,18 @@ package com.example.practica1botones;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Activity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_3);
     }
 
-    public void ventana2(View view) {
-        Intent intent = new Intent(this,Activity2.class);
-        startActivity(intent);
+    public void Ventana2(View view) {
+        finish();
     }
 }
