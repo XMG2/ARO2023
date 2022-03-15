0009a8858d8e2600ea6c78c6a91cdd6da5d90653
xabie <xabiermikelgutierrez@gmail.com>
2022-03-15 16:28:01 +0100

    app version alpha 0.18

--- MyApplication/app/src/main/java/com/example/testListMaterialMenuFloatButton/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/testListMaterialMenuFloatButton/MainActivity.java
@@ -68,7 +68,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+//      /\
+//     /  \
+//   (|  o |)
+//    |    |
+//    |    |
+//    L____|
+//    ^^^^^^
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
