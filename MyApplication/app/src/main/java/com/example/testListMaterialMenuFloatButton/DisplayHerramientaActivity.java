f7ff7da93a9ac0279eefab40f3df9adf1b8ea8a4
xabie <xabiermikelgutierrez@gmail.com>
2022-03-17 13:01:32 +0100

    app version alpha 0.20 porfin creo que se hacer fragments fragment herramienta display hecho

--- MyApplication/app/src/main/java/com/example/testListMaterialMenuFloatButton/DisplayHerramientaActivity.java
+++ MyApplication/app/src/main/java/com/example/testListMaterialMenuFloatButton/DisplayHerramientaActivity.java
@@ -3,17 +3,32 @@ package com.example.testListMaterialMenuFloatButton;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.testListMaterialMenuFloatButton.Modelos.Herramienta;
 
 public class DisplayHerramientaActivity extends AppCompatActivity {
-
+    private ElementosDeViajeApp eva;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_herramienta);
+        eva = (ElementosDeViajeApp) getApplicationContext();
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        int position = bundle.getInt("position");
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
-                .replace(R.id.fragmentContainerViewHerramienta,HerramientaDisplayFragment.class,null)
+                .replace(R.id.fragmentContainerViewHerramienta,new HerramientaDisplayFragment((Herramienta) eva.elementoList.get(position)),null)
                 .setReorderingAllowed(true).commit();
+
+    }
+    public void volver(View view) {
+        Intent intent = new Intent();
+        setResult(Activity.RESULT_CANCELED, intent);
+        finish();
     }
 }
--- MyApplication/app/src/main/java/com/example/testListMaterialMenuFloatButton/HerramientaDisplayFragment.java
+++ MyApplication/app/src/main/java/com/example/testListMaterialMenuFloatButton/HerramientaDisplayFragment.java
@@ -7,12 +7,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HerramientaDisplayFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.testListMaterialMenuFloatButton.Modelos.Herramienta;
+
+
 public class HerramientaDisplayFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -21,44 +20,35 @@ public class HerramientaDisplayFragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
+    private String nombre,descripcion,funcion;
+    private int cantidad;
     public HerramientaDisplayFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HerramientaDisplayFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HerramientaDisplayFragment newInstance(String param1, String param2) {
-        HerramientaDisplayFragment fragment = new HerramientaDisplayFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public HerramientaDisplayFragment(Herramienta herramienta){
+        nombre = herramienta.getNombre();
+        descripcion = herramienta.getDescripcion();
+        funcion = ""+herramienta.getFuncion();
+        cantidad = herramienta.getCantidad();
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_herramienta_display, container, false);
+        View view = inflater.inflate(R.layout.fragment_herramienta_display,container,false);
+        TextView textView1 = (TextView)view.findViewById(R.id.nombreHerramienta);
+        TextView textView2 = (TextView)view.findViewById(R.id.descripcionHerramienta);
+        TextView textView3 = (TextView)view.findViewById(R.id.funcionHerramienta);
+        TextView textView4 = (TextView)view.findViewById(R.id.cantidadHerramienta);
+        textView1.setText(nombre);
+        textView2.setText(descripcion);
+        textView3.setText(funcion);
+        textView4.setText(""+cantidad);
+        return view;
     }
+
 }
