3ac028d9b3faf6ebaef3e6e51536426bea34605e
xabie <xabiermikelgutierrez@gmail.com>
2022-03-17 17:26:54 +0100

    app version alpha 0.23 ya soy capaz de saber q tab esta seleccionada faltan los fragments de modificar

--- MyApplication/app/src/main/java/com/example/testListMaterialMenuFloatButton/DisplayActivity.java
+++ MyApplication/app/src/main/java/com/example/testListMaterialMenuFloatButton/DisplayActivity.java
@@ -1,24 +1,57 @@
 package com.example.testListMaterialMenuFloatButton;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TableLayout;
 
 import com.example.testListMaterialMenuFloatButton.Modelos.Elemento;
 import com.example.testListMaterialMenuFloatButton.Modelos.ElementoProducido;
 import com.example.testListMaterialMenuFloatButton.Modelos.Herramienta;
 import com.example.testListMaterialMenuFloatButton.Modelos.MateriaPrima;
+import com.google.android.material.tabs.TabLayout;
 
 public class DisplayActivity extends AppCompatActivity {
     private ElementosDeViajeApp eva;
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch(tabLayout.getSelectedTabPosition()){
+                    case 0:
+                        System.out.println("heyyy");
+                        break;
+                    case 1:
+                        System.out.println("asdfasdfasd");
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
         eva = (ElementosDeViajeApp) getApplicationContext();
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
@@ -27,19 +60,19 @@ public class DisplayActivity extends AppCompatActivity {
         FragmentManager fragmentManager = getSupportFragmentManager();
         switch (elemento.getTipo(1)){
             case "HERRAMIENTA":
-                System.out.println("herramienta");
+
                 fragmentManager.beginTransaction()
                         .replace(R.id.fragmentContainerView,new HerramientaDisplayFragment((Herramienta) elemento),null)
                         .setReorderingAllowed(true).commit();
                 break;
             case "ELEMENTOPRODUCIDO":
-                System.out.println("producido");
+
                 fragmentManager.beginTransaction()
                         .replace(R.id.fragmentContainerView,new ProducidoDisplayFragment((ElementoProducido) elemento),null)
                         .setReorderingAllowed(true).commit();
                 break;
             case "MATERIAPRIMA":
-                System.out.println("materia");
+
 
                 fragmentManager.beginTransaction()
                         .replace(R.id.fragmentContainerView,new MateriaPrimaDisplayFragment((MateriaPrima) eva.elementoList.get(position)),null)
@@ -48,6 +81,18 @@ public class DisplayActivity extends AppCompatActivity {
                 break;
 
         }
+        //System.out.println("llego");
+
+        //System.out.println("asdfasdf: "+i);
+        /*TabLayout.Tab tab1 = tabLayout.getTabAt(0);
+        TabLayout.Tab tab2 = tabLayout.getTabAt(2);
+        if(tab1.isSelected()){
+            System.out.println("heyyyy");
+        }else if(tab2.isSelected()){
+            System.out.println("ooooooooooo");
+        }*/
+
+
 
 
     }
